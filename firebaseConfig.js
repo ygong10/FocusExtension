619ce65c2f45f56db474d3fe0a8626dced2cd7bd
Yicheng Gong <ygongdev@gmail.com>
2017-03-05 17:15:21 -0600

    removing credentials from public

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var config = {
-	apiKey: "AIzaSyAL517jiGa6Xfv8YW6v8247Qc7mWLbcDp4",
-	authDomain: "focus-fc061.firebaseapp.com",
-	databaseURL: "https://focus-fc061.firebaseio.com",
-	storageBucket: "focus-fc061.appspot.com",
-	messagingSenderId: "1064226865545"
-};
